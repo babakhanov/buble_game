2e75cb7ae0668cb76ce013a41e5395773181ba85
babakhanov <babakhanov1@gmail.com>
2017-03-04 14:51:13 +0200

    add styles

--- src/main.js
+++ src/main.js
@@ -1,3 +1,15 @@
-require("./css/application.scss");
-require("./js/app.js");
+require("./stylesheets/application.scss");
+require("./app/app.js");
+
+import ready from "./app/utils/ready";
+import app from "./app/app";
+
+ready(() => {
+  app();
+});
+
+window.toggleSidebar = () => {
+  document.getElementsByTagName("body")[0].classList.toggle("sidebar-close");
+  app();
+};
 
